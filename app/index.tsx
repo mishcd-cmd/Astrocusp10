ebca726a9840a1ef2d8eb12f5cec12f761b1a3f5
mishcd-cmd <mish.cd@gmail.com>
2025-12-10 17:32:19 +1100

    index

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 // app/index.tsx
 import React from 'react';
-import WelcomeScreen from './settings/welcome';
+import WelcomeScreen from './welcome';
 
 export default function Index() {
   // When someone visits /, show the proper welcome + "Try the Cusp Calculator"
